03d1fef194f032245a5aefe730b3cc37e0d797c1
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2024-05-18 23:10:27 +0530

    readme and css configs

---
+++ app/fonts.ts
@@ -0,0 +1,4 @@
+import { Dongle, Nunito_Sans } from 'next/font/google'
+
+export const dongle = Dongle({subsets:['latin'], style:['normal'], weight:"400"});
+export const nunito = Nunito_Sans({subsets:['latin'], style:['normal'], weight:"400"});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,24 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor:{
+        "primary-dark-blue":"#03001C",
+        "primary-purple": "#85586F",
+        "primary-pink" : "#BB4C85",
+        "primary-dark-purple": "#571738",
+        "neutral-dark-blue" : "#01000D",
+        "neutral-gray-white": "#D9D9D9",
+      },
+      colors:{
+        "primary-black":"#000000",
+        "primary-white": "#FFFFFF"
+      },
+      blur:{
+        "thirteen-percent": "13%",
+        "forty-percent": "40%",
+        "seventyfive-percent": "75%"
+      }
     },
   },
   plugins: [],
